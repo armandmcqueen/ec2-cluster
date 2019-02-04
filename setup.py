5a82c49fefef9a5a114c5aa6955f46d8d37aed43
armandmcqueen <armandmcqueen@gmail.com>
2019-02-03 17:01:21 -0800

    Updated github url on PyPi and incremented version to 0.0.0.a2

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ec2_cluster",
-    version="0.0.0a1",
+    version="0.0.0a2",
     author="Armand McQueen",
     author_email="armandmcqueen@gmail.com",
     description="A tool for launching and running commands on a cluster of EC2 instances",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/armandmcqueen/ec2_cluster",
+    url="https://github.com/armandmcqueen/ec2-cluster",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.6"
