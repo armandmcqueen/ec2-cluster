0597ff93a9fbcae7cca548ec5bd9914358e88c2b
Armand McQueen <armandmcqueen@gmail.com>
2019-05-13 21:56:02 -0700

    Release 0.3.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
         name="ec2_cluster",
-        version="0.3.0",
+        version="0.3.1",
         author="Armand McQueen",
         author_email="armandmcqueen@gmail.com",
         description="A tool for launching and running commands on a cluster of EC2 instances",
