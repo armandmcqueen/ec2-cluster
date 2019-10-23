d1df77318ebb306e560d3c83934be1014a3edc00
armandmcqueen <armandmcqueen@gmail.com>
2019-10-23 01:41:16 -0700

    Read version from a file

--- ec2_cluster/__init__.py
+++ ec2_cluster/__init__.py
@@ -1,2 +1,6 @@
 name = "ec2-cluster"
 
+from pathlib import Path
+version_file = Path(__file__).parent/"VERSION"
+version = version_file.open('r').read().strip()
+
--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 import setuptools
+from pathlib import Path
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+version_file = Path(__file__).parent/"ec2_cluster"/"VERSION"
+VERSION = version_file.open('r').read().strip()
+
 setuptools.setup(
         name="ec2_cluster",
-        version="0.3.2",
+        version=VERSION,
         author="Armand McQueen",
         author_email="armandmcqueen@gmail.com",
-        description="A tool for launching and running commands on a cluster of EC2 instances",
+        description="A tool for launching and running commands on multiple EC2 instances",
         long_description=long_description,
         long_description_content_type="text/markdown",
         url="https://github.com/armandmcqueen/ec2-cluster",
